c3a241104662cabc723e44aef7cea6e5dad6da39
sorenammd <mohammad01293@gmail.com>
2022-11-26 19:12:51 +0330

    .

---
+++ frontend_react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { About , Footer, Header,Skills,Testimonial,Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
+export default function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header /> 
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial /> 
+      <Footer />
+    </div>
+  )
+}
